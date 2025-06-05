89fcc1762902d6a4b4ba77f417747bffd0b59658
yoshi-isle <joshuajaworowski+1@gmail.com>
2025-06-05 00:25:44 -0400

    Refactor player cog to use BASE_API_URL for API calls; enhance image handling in current_tile command and clean up constants

--- discord_bot/cogs/player_cog.py
+++ discord_bot/cogs/player_cog.py
@@ -4,15 +4,15 @@ from discord.ext import commands
 from discord import Embed, app_commands
 import aiohttp
 import os
-
+import io
+from constants import BASE_API_URl
 class PlayerCog(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
     @app_commands.command(name="current_tile", description="Displays your current tile.")
     async def current_tile(self, interaction: discord.Interaction):
-        api_url = os.getenv("GAME_API_URL", f"http://game_service_api:5000/teams/discord/{interaction.user.id}/current_tile")
-        api_base_url = api_url.split('/teams/')[0]
+        api_url = f"{BASE_API_URl}/teams/discord/{interaction.user.id}/current_tile"
         async with aiohttp.ClientSession() as session:
             async with session.get(api_url) as resp:
                 if resp.status == 200:
@@ -24,13 +24,20 @@ class PlayerCog(commands.Cog):
                     )
                     embed.add_field(name="Completion Counter", value=tile.get('completion_counter', 'N/A'))
                     image_url = tile.get('image_url')
+                    file = None
                     if image_url:
-                        # Compose the full image URL
-
-                        print("http://game_service_api:5000/images/{image_url}")
-                        embed.set_image(url=f"http://game_service_api:5000/images/{image_url}")
+                        image_api_url = f"{BASE_API_URl}/images/{image_url}"
+                        async with session.get(image_api_url) as img_resp:
+                            if img_resp.status == 200:
+                                img_bytes = await img_resp.read()
+                                filename = os.path.basename(image_url)
+                                file = discord.File(fp=discord.File(io.BytesIO(img_bytes), filename=filename).fp, filename=filename)
+                                embed.set_image(url=f"attachment://{filename}")
                     embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-                    await interaction.response.send_message(embed=embed)
+                    if file:
+                        await interaction.response.send_message(embed=embed, file=file)
+                    else:
+                        await interaction.response.send_message(embed=embed)
                 else:
                     error = await resp.text()
                     embed = discord.Embed(
